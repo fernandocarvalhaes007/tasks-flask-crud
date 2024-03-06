9a6dcb289d38a72dca7521ecf865c65fd5813ed2
Fernandocarvalhaes007 <fernandosantos07@gmail.com>
2024-03-06 16:25:42 -0300

    Adding tests & Updating created endpoint

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from hmac import new
 from flask import Flask, jsonify, request
 from models.task import Task
 
@@ -14,7 +15,7 @@ def create_task():
     task_id_control += 1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "Nova tarefa criada com sucesso!"})
+    return jsonify({"message": "Nova tarefa criada com sucesso!", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
